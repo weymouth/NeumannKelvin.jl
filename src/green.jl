ca5d77dcfa710ef5c466fc9cf38ec229a299f988
Gabriel Weymouth <drweymouth@google.com>
2024-03-04 22:18:45 +0100

    New function was allocating a lot, killing the speedup.

--- src/green.jl
+++ src/green.jl
@@ -11,7 +11,7 @@ Green Function `G(ξ)` for a source at position `α` moving with `Fn≡U/√gL` 
 the free-surface. The free surface is ζ=0, the coordinates are scaled by L and
 the apparent velocity direction is Û=[-1,0,0]. See Noblesse 1981 for details.
 """
-function kelvin(ξ,α;Fn=1)
+function kelvin(ξ,α;Fn=1,kwargs...)
     # Froude number scaled distances from the source's image
     image = SA[α[1],α[2],-α[3]]
     x,y,z = (ξ-image)/Fn^2
@@ -21,7 +21,7 @@ function kelvin(ξ,α;Fn=1)
     z ≥ -0.01 && throw(DomainError(z,"Kelvin scaled vertical distance z>-0.01"))
 
     # Return source, nearfield, and wavelike disturbance
-	return source(ξ,α)+(1/hypot(x,y,z)+nearfield(x,y,z)+wavelike(x,y,z))/Fn^2
+	return source(ξ,α)+(1/hypot(x,y,z)+nearfield(x,y,z)+wavelike(x,y,z;kwargs...))/Fn^2
 end
 
 using Base.MathConstants: γ
@@ -35,16 +35,15 @@ end
 using QuadGK
 # Wave-like disturbance
 function wavelike(x,y,z;rtol=1e-4,xgl=xgl128,wgl=wgl128,GK=false)
-    x≥0 && return 0
-    Wi(t) = exp(z*(1+t^2))*sin((x+abs(y)*t)*hypot(1,t))
-    if abs(y)<-20z && !GK
-        b = √(-3log(10)/z)
-        return 4b*quadgl(t->Wi(b*t);xgl,wgl)
-    elseif z ≤ -0.025 && !GK
-        b = √(10π/abs(y))
-        dg4(x,y,t) = (x*t+y*(2*t^2+1))^4/(1+t^2)^2
-        sᵧ = dg4(x,y,-b)+dg4(x,y,b)
-        return 8b*quadgl(t->Wi(2b*t)*exp(-dg4(x,y,t)/sᵧ);xgl,wgl)    
-    end
-    4/√-z*quadgk(t->Wi(t/√-z),-Inf,Inf;rtol)[1]
+    x≥0 && return 0.
+    (GK || z > -0.025) && return 4/√-z*quadgk(t->Wi(x,y,z,t/√-z),-Inf,Inf;rtol)[1]
+    abs(y)<-20z ? wavegl(x,y,z,xgl,wgl) : damped(x,y,z,xgl,wgl)
 end
+wavegl(x,y,z,xgl,wgl) = (b=√(-3log(10)/z); 4b*quadgl(t->Wi(x,y,z,b*t);xgl,wgl))
+function damped(x,y,z,xgl,wgl)
+    b = √(10π/abs(y))
+    dg4(x,y,t) = (x*t+abs(y)*(2*t^2+1))^4/(1+t^2)^2
+    scale = dg4(x,y,-b)+dg4(x,y,b)
+    8b*quadgl(t->Wi(x,y,z,2b*t)*exp(-dg4(x,y,t)/scale);xgl,wgl)
+end
+Wi(x,y,z,t) = exp(z*(1+t^2))*sin((x+abs(y)*t)*hypot(1,t))

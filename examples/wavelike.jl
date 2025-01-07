803eb940e88a2281e53da6858007d55ae992d4d4
weymouth <gabriel.weymouth>
2025-01-07 08:22:26 +0100

    Real-line quad fixes
    Ensure a consistent number of real-line quadrature points, and updated the plots. (20 point Gauss-Legendre is plenty.)

--- examples/wavelike.jl
+++ examples/wavelike.jl
@@ -27,22 +27,20 @@ function bruteW(x,y)
 	4quadgk(Wi,-Inf,Inf,rtol=1e-10,atol=1e-10)[1]
 end
 
-function precise_rng(x,y,Δg=4π;nleg=20,nkon=Inf,nlag=5,R=8log(10)-1)
+function precise_rng(x,y,Δg=4π;nleg=0,nkon=Inf,nlag=5,R=8log(10)-1)
     S = stationary_points(x,y)
     a = S[1]
     ρ₀ = Δg*√(inv(0.5Δg*abs(x)+y^2)+inv(x^2+Δg*abs(y)))
     ρₐ = refine_ρ(a,t->g(x,y,t),t->dg(x,y,t),ρ₀;s=-1,Δg,itmx=30,rtol=1e-4)
-    # rngs,mid = [(a-ρₐ,a),(a,a+ρₐ)],a
-    rngs = ((a-ρₐ,a+ρₐ),)
+    rngs = ((a-ρₐ,a),(a,a+ρₐ))
  
     if length(S)==2 && S[2]<R
         b = S[2]
         ρᵦ = refine_ρ(b,t->g(x,y,t),t->dg(x,y,t),ρ₀;s= 1,Δg,itmx=30,rtol=1e-4)
-        # rngs = [(a-ρₐ,a+ρₐ),(b-ρᵦ,b+ρᵦ)]
-        # mid = (rngs[1][2]+rngs[2][1])/2
-        # (rngs[1][2]≥rngs[2][1] || 2abs(g(x,y,a)-g(x,y,b))≥Δg) && (rngs = [(rngs[1][1],mid),(mid,rngs[2][2])])
-        rngs = (rngs...,(b-ρᵦ,b+ρᵦ))
-        (rngs[1][2]≥rngs[2][1] || 2abs(g(x,y,a)-g(x,y,b))≥Δg) && (rngs = ((rngs[1][1],rngs[2][2]),))
+        # r = (b-a)/(ρₐ+ρᵦ)*min(1,2Δg/abs(g(x,y,a)-g(x,y,b)))
+        # rngs = ((a-ρₐ,a+ρₐ*r),(b-ρᵦ*r,b+ρᵦ))
+        mid = (a-ρₐ+b+ρᵦ)/2
+        rngs = (a+ρₐ ≥ b-ρᵦ || abs(g(x,y,a)-g(x,y,b))≥Δg) ? ((a-ρₐ,mid),(mid,b+ρᵦ)) : ((a-ρₐ,a+ρₐ),(b-ρᵦ,b+ρᵦ))
     end
 
     # Compute real-line contributions
@@ -52,27 +50,38 @@ function precise_rng(x,y,Δg=4π;nleg=20,nkon=Inf,nlag=5,R=8log(10)-1)
         u,v = reim(ĝ(t))
         exp(-v)*sin(u)
     end
-    # xleg,wleg = gausslegendre(nleg)
-    # I = sum(quadgl_ab(f,rng...;x=xleg,w=wleg) for rng in rngs)
-    I = sum(quadgk(f,rng...;order=8,atol=1e-10,maxevals=2*nkon+1)[1] for rng in rngs)
+    I = if nleg>0 # use fixed-size Gauss-Legendre
+        xleg,wleg = gausslegendre(nleg)
+        sum(quadgl_ab(f,rng...;x=xleg,w=wleg) for rng in rngs)
+    else          # use adaptive Gauss-Konrad
+        sum(quadgk(f,rng...;atol=1e-10,maxevals=15*nkon)[1] for rng in rngs)
+    end
 
     # Add the end point contributions
     xlag,wlag = gausslaguerre(nlag)
-    for rng in rngs, (i,t₀) in enumerate(rng)
-        # t₀ ≠ mid && (I+= (-1)^i*nsp(t₀,ĝ,dĝ;xlag,wlag))
-        I+= (-1)^i*nsp(t₀,ĝ,dĝ;xlag,wlag)
+    for rng in merge(rngs...), (i,t₀) in enumerate(rng)
+        I += (-1)^i*nsp(t₀,ĝ,dĝ;xlag,wlag)
     end
     4I
 end
+using Base: merge
+Base.merge(a::NTuple{2},b::NTuple{2}) = a[2]+sqrt(eps())≥b[1] ? ((a[1],b[2]),) : (a,b)
+Base.merge(a::NTuple{2}) = (a,)
 
 Δg = 0.3:0.05:6pi
 cmap = cgrad(:matter, 5, categorical = true);
 x,y = -110,30
 plot(xlabel="Δg",ylabel="|error|",yscale=:log10,ylim=(1e-10,1));
-for (i,np) in enumerate((8,16,32,64))
-    plot!(Δg,g->abs(precise_rng(x,y,g;nkon=np)-bruteW(x,y)),label=np,c=cmap[i]);
+for (i,np) in enumerate((1,3,7))
+    plot!(Δg,g->abs(precise_rng(x,y,g;nkon=np)-bruteW(x,y)),label=30np,c=cmap[i]);
 end;plot!(legendtitle="Gauss-Konrad points",legendtitlefontsize=9)
 savefig("Gauss-Konrad points.png")
+
+plot(xlabel="Δg",ylabel="|error|",yscale=:log10,ylim=(1e-10,1));
+for (i,np) in enumerate((15,20,25))
+    plot!(Δg,g->abs(precise_rng(x,y,g;nleg=np)-bruteW(x,y)),label="2*$np",c=cmap[i]);
+end;plot!(legendtitle="Gauss-Legendre points",legendtitlefontsize=9)
+savefig("Gauss-Legendre points.png")
 
 plot(xlabel="Δg",ylabel="|error|",yscale=:log10,ylim=(1e-10,1));
 for (i,np) in enumerate(2:6)

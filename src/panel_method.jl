7c570aded398d686724d82682b1ad5cd5c047eb8
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2025-02-11 10:38:21 +0100

    Rename total potential Φ
    Unicode phi and varphi are rendered interchangably, which is too confusing. Now using Φ as the summed potential over the panels.
    
    Also made `ϕ=∫G` an argument in Φ and ∂ₙϕ for clarity and cleanliness.

--- src/panel_method.jl
+++ src/panel_method.jl
@@ -33,19 +33,12 @@ function ∫G(d::AbstractVector{<:Dual{Tag}},p;d²=4,kwargs...) where Tag
 end
 _∫G(ξ,p;d²) = sum(abs2,ξ-p.x)>d²*p.dA ? p.dA*source(ξ,p.x) : 0.25p.dA*sum(source(ξ,x) for x in p.x₄)
 
-"""
-    ϕ(x,p;∫G,kwargs...)
-
-Disturbance potential `∫G(x,p)` of panel `p` on point `x`.
-"""
-ϕ(x,p;∫G=∫G,kwargs...) = ∫G(x,p;kwargs...)
-
 """ 
-    ∂ₙϕ(pᵢ,pⱼ;kwargs...) = Aᵢⱼ
+    ∂ₙϕ(pᵢ,pⱼ;ϕ=∫G,kwargs...) = Aᵢⱼ
 
 Normal velocity influence of panel `pⱼ` on `pᵢ`.
 """
-∂ₙϕ(pᵢ,pⱼ;kwargs...) = derivative(t->ϕ(pᵢ.x+t*pᵢ.n,pⱼ;kwargs...),0.)::Float64
+∂ₙϕ(pᵢ,pⱼ;ϕ=∫G,kwargs...) = derivative(t->ϕ(pᵢ.x+t*pᵢ.n,pⱼ;kwargs...),0.)::Float64
 
 """ 
    influence(panels;kwargs...) = ∂ₙϕ.(panels,panels';kwargs...) = A
@@ -61,31 +54,31 @@ function influence!(A,panels;kwargs...)
     end; A
 end
 """
-    φ(x,q,panels;kwargs...)
+    Φ(x,q,panels;ϕ=∫G,kwargs...)
 
-Potential `φ(x) = ∫ₛ q(x')G(x-x')ds' = ∑ᵢqᵢϕ(x,pᵢ)` of `panels` with strengths `q`.
+Potential `Φ(x) = ∫ₛ q(x')ϕ(x-x')ds' = ∑ᵢqᵢϕ(x,pᵢ)` of `panels` with strengths `q`.
 Computation is accelerated with multi-threading when `Threads.nthreads()>1`.
 """
-φ(x,q,panels;kwargs...) = ThreadsX.sum(qᵢ*ϕ(x,pᵢ;kwargs...) for (qᵢ,pᵢ) in zip(q,panels))
-∇φ(x,q,panels;kwargs...) = gradient(x->φ(x,q,panels;kwargs...),x)
-ζ(x,y,q,panels;kwargs...) = derivative(x->φ(SVector(x,y,0),q,panels;kwargs...),x)
+Φ(x,q,panels;ϕ=∫G,kwargs...) = ThreadsX.sum(qᵢ*ϕ(x,pᵢ;kwargs...) for (qᵢ,pᵢ) in zip(q,panels))
+∇Φ(x,q,panels;kwargs...) = gradient(x->Φ(x,q,panels;kwargs...),x)
+ζ(x,y,q,panels;kwargs...) = derivative(x->Φ(SVector(x,y,0),q,panels;kwargs...),x)
 
 """
     steady_force(q,panels,U=SVector(-1,0,0);kwargs...)
 
 Integrated pressure force coefficient Cₚ =∫ₛ cₚ n da of `panels` with strengths `q`.
-where cₚ = 1-u²/U², `U` is the freestreeam velocity and u=U+∇φ is the flow velocity.
+where cₚ = 1-u²/U², `U` is the freestreeam velocity and u=U+∇Φ is the flow velocity.
 Computation is accelerated with multi-threading when `Threads.nthreads()>1`.
 """
 steady_force(q,panels;U=SVector(-1,0,0),kwargs...) = ThreadsX.sum(panels) do pᵢ
-    u² = sum(abs2,U+∇φ(pᵢ.x,q,panels;kwargs...))
+    u² = sum(abs2,U+∇Φ(pᵢ.x,q,panels;kwargs...))
     cₚ = 1-u²/sum(abs2,U)
     cₚ*pᵢ.n*pᵢ.dA
 end
 """
     added_mass(panels;kwargs...)
 
-Added mass matrix mᵢⱼ = -∫ₛ φᵢ(x) nⱼ da for a set of `panels`, where φᵢ is the 
+Added mass matrix mᵢⱼ = -∫ₛ Φᵢ(x) nⱼ da for a set of `panels`, where Φᵢ is the 
 potential due to unit motion in direction i.
 Computation is accelerated with multi-threading when `Threads.nthreads()>1`.
 """
@@ -93,5 +86,5 @@ function added_mass(panels;kwargs...)
     A = influence(panels;kwargs...)
     B = panels.n |> stack # source _matrix_ over i=1,2,3
     Q = A\B' # solution _matrix_ over i=1,2,3
-    [-ThreadsX.sum(p->φ(p.x,view(Q,:,i),panels;kwargs...)*p.n[j]*p.dA,panels) for i in 1:3, j in 1:3]
+    [-ThreadsX.sum(p->Φ(p.x,view(Q,:,i),panels;kwargs...)*p.n[j]*p.dA,panels) for i in 1:3, j in 1:3]
 end

f170aace2cf1e5fe319c5cdd4ce73db9f6d01017
weymouth <gabriel.weymouth@gmail.com>
2025-02-16 11:25:40 +0100

    Update wavelike.jl
    Added transverse wake slice plot. Trouble when z goes to -0.

--- examples/wavelike.jl
+++ examples/wavelike.jl
@@ -1,23 +1,32 @@
 using NeumannKelvin,StaticArrays
+using NeumannKelvin: wavelike
+using SpecialFunctions
 using Plots
 
 x = -20:0.01:-1
-plot(x,x->NeumannKelvin.wavelike(x,0.,-0.),xlabel="x",ylabel="W(x,0,0)",label="")
+plot(x,x->wavelike(x,0.,-0.),xlabel="x",ylabel="W(x,0,0)",label="")
 plot!(x,x->4π*bessely1(-x),label="",lc=:grey,ls=:dash)
 savefig("wavelike_centerline.png")
 
 x = -40:0.01:-1
-plot(x,x->NeumannKelvin.wavelike(x,abs(x/(2√2)),-0.),label="")
+plot(x,x->wavelike(x,abs(x/(2√2)),-0.),label="")
 plot!(x,x->11hypot(x,x/(2√2))^(-1/3),lc=:grey,label="",xlabel="x")
 plot!(x,x->-11hypot(x,x/(2√2))^(-1/3),lc=:grey,label="",ylabel="W(x,x/2√2,0)")
 savefig("wavelike_kelvin_angle.png")
 
-z = log.(0.01:0.01:1)
+z = @. -abs(log(0.01:0.01:1))
 plot(z,z->4exp(z)*√(-π*z),lc=:grey,xlabel="z",ylabel="zW(x,0,z)",label="")
-plot!(z,z .*NeumannKelvin.wavelike.(-1.2,0.,z),lc=:darkgreen,label="x=-1.2")
-plot!(z,z .*NeumannKelvin.wavelike.(-1,0.,z),lc=:green,label="x=-1")
-plot!(z,z .*NeumannKelvin.wavelike.(-0.8,0.,z),lc=:seagreen,label="x=-0.8")
+plot!(z,z .* wavelike.(-1.2,0.,z),lc=:darkgreen,label="x=-1.2")
+plot!(z,z .* wavelike.(-1,0.,z),lc=:green,label="x=-1")
+plot!(z,z .* wavelike.(-0.8,0.,z),lc=:seagreen,label="x=-0.8")
 savefig("wavelike_vertical.png")
+
+y = logrange(0.1,5,500)
+plot(y,wavelike.(-10.,y,-0.1),lc=:darkgreen,label="z=-0.1")
+plot!(y,wavelike.(-10.,y,-0.03),lc=:green,label="z=-0.03")
+plot!(y,wavelike.(-10.,y,-0.01),lc=:seagreen,label="z=-0.01")
+plot!(xlabel="y",ylabel="W(-1,y,z)")
+savefig("wavelike_transverse.png")
 
 using NeumannKelvin: g,dg,nsp,refine_ρ,quadgl,stationary_points,combine
 using FastGaussQuadrature, QuadGK, Plots, Roots

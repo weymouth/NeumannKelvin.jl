6a00e89ccb5aee2aaf7fe51a3eb0602cac1ecca6
Gabriel Weymouth <drweymouth@google.com>
2025-05-28 21:25:54 +0200

    Update viz
    The Makie.viz function now makes each of the panels look rounded using the normal information at each panel corner. This required digging into GeometryBasics.Mesh, but the resulting code is much cleaner and the plot looks great!
    
    Also force Plots.viz to use the right aspect ratio.

--- ext/NeumannKelvinMakieExt.jl
+++ ext/NeumannKelvinMakieExt.jl
@@ -1,26 +1,20 @@
 module NeumannKelvinMakieExt
 using NeumannKelvin,Makie
+using Makie.GeometryBasics
 # Generate full triangle mesh and color array
-function NeumannKelvin.viz(panels::Table, values=panels.dA; vectors=0.3panels.n, colormap=:viridis, kwargs...)
+function NeumannKelvin.viz(panels::Table, values=panels.dA; vectors=0.3panels.n, clims = extrema(values), kwargs...)
     fig = Figure()
     ax = Axis3(fig[1, 1], aspect=:data)
 
-    # Normalize and map to color
-    vmin, vmax = extrema(values)
-    norm_vals = (values .- vmin) ./ (vmax - vmin + eps())
-    colors = cgrad(colormap)[norm_vals]
-
-    # Mesh geometry (triangle list) and colors
-    triangles = mapreduce(vcat, panels) do panel
-        p = panel.⛶
-        [p[1], p[2], p[3], p[4], p[3], p[2]]
-    end
-    tri_colors = mapreduce(c -> fill(c, 6), vcat, colors)
-    mesh!(ax, triangles; color=tri_colors)
+    # Mesh geometry (vertices, normals, and colors)
+    face = decompose(QuadFace{GLIndex},Tessellation(Rect(0,0,1,1),(2,2))) # 2x2 quad type
+    unwrap(a) = Point3f.(map(i->a[i],SA[1,2,4,3])) # flattens our 2x2 data counter-clockwise
+    quad(panel) = GeometryBasics.Mesh(unwrap(panel.xᵤᵥ), face, normal=unwrap(panel.nᵤᵥ))
+    mesh!(ax, quad.(panels); color=values, colorrange=clims, kwargs...)
 
     # Normals & color bar
-    arrows!(ax, components(panels.x)..., components(vectors)...; color=colors)
-    Colorbar(fig[1, 2], limits=(vmin, vmax); colormap, kwargs...)
+    arrows!(ax, components(panels.x)..., components(vectors)...; color=values, colorrange=clims, kwargs...)
+    Colorbar(fig[1, 2];colorrange=clims, kwargs...)
     fig
 end
 end
--- ext/NeumannKelvinPlotsExt.jl
+++ ext/NeumannKelvinPlotsExt.jl
@@ -2,7 +2,11 @@ module NeumannKelvinPlotsExt
 using NeumannKelvin,Plots
 function NeumannKelvin.viz(panels::Table, values=panels.dA; vectors=0.3panels.n, kwargs...)
     @warn "Using fallback Plots-based panel plotting. Load GLMakie or WGLMakie instead for more functionality." maxlog=1
+    (xmn,xmx),(ymn,ymx),(zmn,zmx)=extrema.(components(panels.x))
+    xc,yc,zc = 0.5xmx+0.5xmn,0.5ymx+0.5ymn,0.5zmx+0.5zmn
+    h=1.05max(xmx-xc,ymx-yc,zmx-zc)
     scatter(components(panels.x),marker_z=values;label="",kwargs...)
-    quiver!(components(panels.x),quiver=components(vectors),color=:grey)
+    quiver!(components(panels.x),quiver=components(vectors),color=:grey,
+            xlims=(xc-h,xc+h),ylims=(yc-h,yc+h),zlims=(zc-h,zc+h))
 end
 end

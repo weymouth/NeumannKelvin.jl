0afe8a8bd7d292db35150f3f5b4f68c7190b5edc
Gabriel Weymouth <drweymouth@google.com>
2025-06-07 15:01:04 +0200

    Fix NURBSExt

---
+++ examples/sphere_test.jl
@@ -0,0 +1,10 @@
+using NeumannKelvin
+using Plots
+using NURBS
+using FileIO
+
+patches = load(pkgdir(NURBS) * "/test/assets/sphere.stp")
+h = 0.25
+measure_panel(patches[1],0.5,0.5,1.0,1.0)
+panels = mapreduce(s->panelize(s;hᵤ=h),vcat,patches)
+viz(panels,panels.dA/h^2,colormap=:ice,clims=(0.5,1.5))
--- ext/NeumannKelvinNURBSExt.jl
+++ ext/NeumannKelvinNURBSExt.jl
@@ -2,9 +2,9 @@ module NeumannKelvinNURBSExt
 using NeumannKelvin,NURBS
 
 # NURBSsurface wrapper for scalar parameter arguments
-scalarize(patch::NURBSsurface) = (u, v) -> patch(u, v)[1]
+scalarize(patch) = (u, v) -> patch(u, v)[1]
 
 # Make sure exported panel functions use this wrapper
-panelize(patch::NURBSsurface,args...;kwargs...) = panelize(scalarize(patch),args...;kwargs...)
-measure_panel(patch::NURBSsurface,args...;kwargs...) = measure_panel(scalarize(patch),args...;kwargs...)
+NeumannKelvin.panelize(patch::NURBSsurface,args...;kwargs...) = panelize(scalarize(patch),args...;kwargs...)
+NeumannKelvin.measure_panel(patch::NURBSsurface,args...;kwargs...) = measure_panel(scalarize(patch),args...;kwargs...)
 end

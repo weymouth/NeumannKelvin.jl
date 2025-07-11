66ff15b27a2e0881fc5dc436fe54d243fdcedb72
weymouth <gabriel.weymouth>
2025-07-11 14:13:29 +0200

    Update prism example

--- examples/elliptical_prism.jl
+++ examples/elliptical_prism.jl
@@ -1,39 +1,45 @@
-using NeumannKelvin,Plots
+using NeumannKelvin,Plots,ColorSchemes
 ∫kelvin₂ = reflect(∫kelvin,2)
 Cw(panels;kwargs...) = 2steady_force(influence(panels;kwargs...)\first.(panels.n),panels;kwargs...)[1]
 
 ∫kelvin₀(x,p;kwargs...) = 2∫kelvin(x,reflect(p,SA[1,0,1]);kwargs...)
 Cw₀(panels;kwargs...) = 2steady_force(first.(panels.n)/4π,panels;kwargs...)[1]
 
-function prism(h;q=0.2,Z=1,r=1.2)
-    S(θ,z) = 0.5SA[cos(θ),q*sin(θ),z]
-    dθ = π/round(π*0.5/h) # cosine sampling
-    mapreduce(vcat,0.5dθ:dθ:π) do θ
-        dx = 1.618dθ*hypot(q*cos(θ),sin(θ))
-        i = round(log(1+2Z/dx*(r-1))/log(r)) # geometric growth
-        mapreduce(vcat,1:i) do j
-            z,dz = -dx*(1-r^j)/(1-r),dx*r^(j-1)
-            measure_panel.(S,θ,z+0.5dz,dθ,dz)
-        end
-    end |> Table
+function prism(hᵤ,hᵥ=1.618hᵤ;q=0.2,Z=2,c=0.05,r=1.2)
+    # Define prism surface
+    surface(u,v) = SA[0.5cos(π*u),0.5q*sin(π*u),-v]
+    # Define WL segments based on pseudo-arclength
+    S,s⁻¹ = NeumannKelvin.arclength(u->surface(u,0),hᵤ,c,0,1)
+    Nᵤ = round(Int,S/hᵤ)
+    u = s⁻¹(range(0,S,Nᵤ+1))
+    uc,du = 0.5*(u[1:end-1]+u[2:end]), diff(u)
+    # Define vertical segments with geometric growth
+    Nᵥ = round(Int,log(1+Z/hᵥ*(r-1))/log(r))
+    v = @. hᵥ*(1-r^(0:Nᵥ))/(1-r)
+    vc,dv = 0.5*(v[1:end-1]+v[2:end]), diff(v)
+    # Measure each panel
+    measure_panel.(surface,uc,vc',du,dv',flip=true) |> Table
 end
 panels = prism(0.05)
 
 dat = map(logrange(0.2,0.6,40)) do Fn
-    (Fn=Fn,Cw=Cw(panels,ϕ=∫kelvin₂,ℓ=Fn^2))
+    (Fn=Fn,Contour=Cw(panels,ϕ=∫kelvin₂,ℓ=Fn^2,contour=true,filter=false),
+           NoContour=Cw(panels,ϕ=∫kelvin₂,ℓ=Fn^2),
+           ThinShip=Cw₀(panels,ϕ=∫kelvin₀,ℓ=Fn^2),
+           ThinSource=Cw₀(panels,ϕ=∫kelvin₂,ℓ=Fn^2))
 end |> Table
-dat2 = map(logrange(0.2,0.6,40)) do Fn
-    (Fn=Fn,Cw=Cw(panels,ϕ=∫kelvin₂,ℓ=Fn^2,contour=true,filter=false))
-end |> Table
-dat3 = map(logrange(0.2,0.6,40)) do Fn
-    (Fn=Fn,Cw=Cw₀(panels,ϕ=∫kelvin₀,ℓ=Fn^2))
-end |> Table
-dat4 = map(logrange(0.2,0.6,40)) do Fn
-    (Fn=Fn,Cw=Cw₀(panels,ϕ=∫kelvin₂,ℓ=Fn^2))
-end |> Table
-
-plot(dat.Fn,dat.Cw,label="No-contour");
-plot!(dat.Fn,dat2.Cw,label="Contour");
-plot!(dat.Fn,dat3.Cw,label="Thin-ship");
-plot!(dat.Fn,dat4.Cw,label="Thin-source")
+plot(dat,ylabel="R/½ρU²L²",size=(400,500),ylims=(0,0.1))
 savefig("examples/ellipical_prism.png")
+
+function WL(panels;kwargs...)
+    x,y,_ = filter(onwaterline,panels) |> p -> components(p.x)
+    q = influence(panels;kwargs...)\components(panels.n,1)
+    z = map(xy->ζ(xy[1],xy[2],q,panels;kwargs...),zip(x,y))
+    x,z,q
+end
+plot(); for (n,c) in zip((40,28,20),reverse(colorschemes[:Blues_4]))
+    panels = prism(1/n,1/2n)
+    x,z = WL(panels; ϕ=∫kelvin₂, ℓ=0.2^2, contour=true, filter=true)
+    plot!(x,z,label="hᵤ=L/$n",line=(2),marker=(2,),markerstrokewidth=0;c)
+end;plot!(xlabel="x/L",ylabel="ζg/U²")
+savefig("examples/ellipical_prismWLconv.png")

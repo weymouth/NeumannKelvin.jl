5492d6ef9c389d7e2249cf9cb61d82d51b5c8844
Gabriel Weymouth <drweymouth@google.com>
2025-07-15 08:45:22 +0200

    Generalize quad functions
    Added a slowly varying function γ to complex_path and nsp. Also cleaned up the finite_ranges logic.

--- src/quad.jl
+++ src/quad.jl
@@ -14,43 +14,40 @@ end
 quadgl(f,a,b;x=SA[-1/√3,1/√3],w=SA[1,1]) = (b-a)/2*quadgl(t->f((b+a)/2+t*(b-a)/2);x,w)
 
 """
-    complex_path(g,dg,rngs,skp)
+    complex_path(g,dg,rngs;atol=1e-3,γ=one,f=Im(γ*exp(im*g)))
 
-Estimate the integral `imag(∫exp(im*g(t))dt)` from `t=[-∞,∞]` using
-a complex path. The finite phase ranges `rngs` are integrated along
-the real line using Gauss-Legendre. The range endpoints where
-`flag=true` are integrated in the complex-plane using the phase
-derivative `dg(t)=g'` and numerical stationary phase.
+Estimate the integral `∫f(t)dt` from `t=[-∞,∞]` using a complex path.
+The finite phase ranges `rngs` are integrated along the real line
+with QuadGK. The range endpoints where `flag=true` are integrated to
+±∞ in the complex-plane using `±nsp(t₀,g,dg,γ)`.
 """
-function complex_path(g,dg,rngs,atol=1e-3)
-    # Make an efficient integrand function for real t
-    @fastmath @inline function f(t)
-        u,v = reim(g(t))
-        exp(-v)*sin(u)
-    end
+function complex_path(g,dg,rngs;atol=1e-3,γ=one,
+    f = t->((u,v)=reim(g(t)); @fastmath γ(t)*exp(-v)*sin(u)))
 
     # Sum the flagged endpoints and interval contributions
     sum(rngs) do ((t₁,∞₁),(t₂,∞₂))
-        (∞₁ ? -nsp(t₁,g,dg) : zero(t₁))+quadgk(f,t₁,t₂;atol)[1]+(∞₂ ? nsp(t₂,g,dg) : zero(t₂))
+        ∫f = f==zero ? f(t₁) : quadgk(f,t₁,t₂;atol)[1]
+        (∞₁ ? -nsp(t₁,g,dg) : zero(t₁)) + ∫f + (∞₂ ? nsp(t₂,g,dg) : zero(t₂))
     end
 end
 
 using Roots
 """
-    nsp(h₀,g,dg)
+    nsp(h₀,g,dg,γ=one)
 
-Integrate the contributions of `imag(∫exp(im*g(h))dh)` from
+Integrate the contributions of `imag(∫γ(h)exp(im*g(h))dh)` from
 `h = [h₀,∞]` using numerical stationary phase. The complex path
-is found as the roots of `ϵ(h)=g(h)-g(h₀)-im*p=0` where `p`
-are Gauss-Laguerre integration points.
+is found as the roots of `ϵ(h)=g(h)-g(h₀)-im*p=0` where `p` are
+Gauss-Laguerre integration points. The pre-function `γ` must be
+slowly varying compared to `g` over `h`.
 """
-@fastmath function nsp(h₀,g,dg;xlag=xlag,wlag=wlag,atol=1e-3)
+@fastmath function nsp(h₀,g,dg,γ=one;xlag=xlag,wlag=wlag,atol=1e-3)
     # Sum over complex Gauss-Laguerre points
     g₀,h = promote(g(h₀),h₀)
     s = zero(typeof(imag(g₀)))
     for (p,w) in zip(xlag,wlag)
         h = find_zero((h->g(h)-g₀-im*p,dg),h,Roots.Newton();atol)
-        s += w*imag(exp(im*g₀)*im/dg(h))
+        s += w*imag(γ(h)*exp(im*g₀)*im/dg(h))
     end;s
 end
 
@@ -76,8 +73,8 @@ function finite_ranges(S,g,Δg,R;atol=0.1Δg)
         ((fz(a,-R),fz(a,R)),)
     else
         a,b = extrema(S)
+        l,r = fz(a,-R),fz(b,R)
         p,q = fz(a,a/2+b/2),fz(b,a/2+b/2)
-        !p[2] && (q=p); !q[2] && (p=q)
-        (fz(a,-R),p),(q,fz(b,R))
+        (p[2] && q[2]) ? ((l,p),(q,r)) : ((l,r),)
     end |> value # ranges can't be Duals
 end

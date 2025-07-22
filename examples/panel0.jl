a8926f9f11c770828502c130dff45eed3e7137e4
weymouth <gabriel.weymouth@gmail.com>
2025-07-22 22:35:33 +0200

    Forced to add the k_y damping back in
    The integration of the panel helps, but the frequency is still far too high. Maybe there is a way to double panel integrate, like a BEM. But the single panel integral is not enough when z=0

--- examples/panel0.jl
+++ examples/panel0.jl
@@ -7,24 +7,7 @@ derivative(z->ϕₙ(z,1e-2),-0.)*1e2
 
 using NeumannKelvin,TupleTools,QuadGK,IntervalSets
 using NeumannKelvin: Δg_ranges,g,kₓ,∫Wᵢ
-"""
-    ∫₂wavelike(x, z, a, b)
 
-Compute the integral `I = 4∫∫Im(exp(i*g(y,t))) dt dy` where `t∈[-∞,∞], y∈[a,b]` and `g(y,t)=(x+y*t)√(1+t²)-i*z*(1+t²)`.
-
-## Implementation Details
-The integration order is swapped and the y-integral is evaluated analytically to give:
-
-    I = 4∫f_b dt - 4∫f_a dt = 4∫Im(exp(i*g(b,t))/itk(t)) dt - 4∫Im(exp(i*g(a,t))/itk(t))
-
-where `kₓ(t) = √(1+t²)`. Each integral has a set of `A,B=finite_ranges(a,b)` and `nsp` can be
-used to integrate to ±∞ away from these. To avoid catastrophic cancellation over the interval
-intersections M = A ∩ B (especially near t=0), the combined integrand is used over these ranges:
-
-    I = 4∫f_m dt = 4∫Δ*sinc(Δ*t*k(t)/2π)*Im(exp(i*g(m,t))) dt
-
-where `Δ,m = (b-a),(a+b)/2`, while `f_a,f_b` are evalauted over `A-M,B-M`.
-"""
 function ∫₂wavelike(x,z,a,b,ltol=-5log(10),atol=10exp(ltol))
     (x≥0 || z≤ltol) && return 0.
     # Get ranges for f_a, f_b
@@ -54,32 +37,50 @@ check(-1.,-0.0,1e-3)
 using Plots
 plot(range(-30,-0,1000),x->∫₂wavelike(x,-0.,-0.5,0.5),label="∫Gdy",xlabel="xg/U²")
 plot(range(-30,-0,10000),x->derivative(z->∫₂wavelike(x,z,-0.5,0.5),-0.),label="∫dG/dz dy",xlabel="xg/U²")
+"""
+    ∫wavelike(z, a, b, c, d)
 
-function ∫wavelike(z,a,b,c,d,ltol=-5log(10),atol=10exp(ltol))
+Compute the integral `I = 4∫∫∫Im(exp(i*g(x,y,t))) dxdydt` where `t∈[-∞,∞], x∈[a,b], y∈[c,s]`
+and `g(x,y,t)=(x+y*t)√(1+t²)-i*z*(1+t²)`.
+
+## Implementation Details
+The x&y-integrals are evaluated analytically to give:
+
+    I = ∑ⱼₖ 4∫Im(fⱼₖ(t)) dt = ∑ⱼₖ 4∫Im(γ(t)*exp(i*g(xⱼ,yₖ,t)))
+
+where `γ(t) = ±1/(t*(1+t²))`. Each integral has a set integration intervals `rngsⱼₖ` and `nsp` 
+is used to integrate to ±∞ away from these. To avoid catastrophic cancellation over the interval
+intersections `M = ⋂ⱼₖ rngsⱼₖ` (especially near t=0), the combined integrand is used over M:
+
+    I = 4∫f_m dt = 4ΔxΔy∫sinc(Δx√(1+t²)/2π)sinc(Δy*t√(1+t²)/2π)*Im(exp(i*g(mx,my,t))) dt
+
+where `Δᵢ,mᵢ` are the difference and mean of the x,y corners, while `fⱼₖ` is used over `rngsⱼₖ-M`.
+"""
+function ∫wavelike(z,a,b,c,d;ltol=-5log(10),atol=10exp(ltol),α=0)
     (a≥0 || z≤ltol) && return 0. # Panel downstream or too deep
-    b = min(-0.,b)               # Heaviside limit
+    b = min(-eps(),b)            # Heaviside limit
 
     # Get ranges
     x,y = SA[a,b],SA[c,d]        # corners
     Δg,R = -0.5ltol,√(ltol/z-1)  # phase width & range limit
     rngs = Δg_ranges.(x,y',Δg,R) # finite phase ranges
     M = reduce(∩,rngs)           # intersection
-    isempty(M) && throw(ArgumentError("Empty intersection: reduce panel size"))
+    isempty(M) && throw(ArgumentError("∫wavelike: Empty intersection - reduce panel size"))
 
     # Range intersections: use f_m & quadgk
     Δx, Δy, mx, my = b-a, d-c, (a+b)/2, (c+d)/2
-    @fastmath f_m(t) = sinc(Δx*kₓ(t)/2π)*sinc(Δy*t*kₓ(t)/2π)*sin(g(mx,my,t))*exp(z*(1+t^2))
+    @fastmath f_m(t) = sinc(Δx*kₓ(t)/2π)*sinc(Δy*t*kₓ(t)/2π)*sin(g(mx,my,t))*exp(z*(1+t^2)-g(0.,α,t)^2)
     all(0 .∉ M) && return Δx*Δy*∫Wᵢ(mx,my,z,M;f=f_m,atol) # can't use γ!
     I_m = 4Δx*Δy*sum(rng->quadgk(f_m,endpoints(rng)...;atol)[1],M)
 
-    # Range differences: use ±γ*fᵢ & ∫Wᵢ
+    # Range differences: use ±γ*f & ∫Wᵢ
     I_m - Δx*Δy*sum(Iterators.product(enumerate(x), enumerate(y))) do ((i,xᵢ), (j,yⱼ))
-        @fastmath γ(t) = (-1)^(i+j)/(t*(1+t^2)*Δx*Δy)
+        @fastmath γ(t) = (-1)^(i+j)/(t*(1+t^2)*Δx*Δy)*exp(-g(0.,α,t)^2)
         @fastmath f(t) = γ(t)*sin(g(xᵢ,yⱼ,t))*exp(z*(1+t^2))
         ∫Wᵢ(xᵢ,yⱼ,z,rngs[i,j]\M; γ,f,atol)
     end
 end
-ϕₖ(z,h) = ∫wavelike(z,-0.5h,0.5h,-0.5h,0.5h,-10log(10))
+ϕₖ(z,h) = ∫wavelike(z,-0.5h,0.5h,-0.5h,0.5h,ltol=-10log(10))
 ϕₖ(-0.,1e-3)*1e6,-π/2
 w₀(h) = derivative(z->ϕₖ(z,h),-0.)
 w₀(1e-3)*1e3,-2
@@ -96,5 +97,18 @@ hcubature(xy->NeumannKelvin.wavelike(xy[1],xy[2],-1.),SA[-33√8,-33],SA[-33√8
 
 using Plots
 plot(range(-30,-0,1000),x->∫wavelike(-0.,x,x+1,-0.5,0.5),label="∫∫Gda",xlabel="xg/U²")
-plot(range(-30,-0,1000),x->derivative(z->∫wavelike(z,x,x+1,-0.5,0.5),-0.),label="∫∫dG/dz da",xlabel="xg/U²")
 plot(range(-30,-0,1000),x->derivative(x->∫wavelike(-0.,x,x+1,-0.5,0.5),x),label="∫∫dG/dx da",xlabel="xg/U²")
+plot(range(-30,-0,1000),x->derivative(z->∫wavelike(z,x,x+1,-0.5,0.5),-0.),label="∫∫dG/dz da",xlabel="xg/U²")
+plot!(range(-30,-0,1000),x->derivative(z->∫wavelike(z,x,x+1,-0.5,0.5,α=0.1),-0.),label="∫∫dG/dz da",xlabel="xg/U²")
+
+plot(ylabel="∫∫dG/dz da",xlabel="xg/U²");for α in (0,0.025,0.05,0.1)
+    plot!(range(-6,6,1000),y->derivative(z ->∫wavelike(z,-5√8,-5√8+1,y-0.5,y+0.5;α),-0.),label=α)
+end;plot!()
+plot(ylabel="∫∫dG/dz da",xlabel="xg/U²");for α in (0,0.025,0.05,0.1)
+    plot!(range(-12,12,1000),y->derivative(z ->∫wavelike(z,-10√8,-10√8+1,y-0.5,y+0.5;α),-0.),label=α)
+end;plot!()
+
+contour(-30:0.5:-0,-15:0.25:15,(x,y)->derivative(z->NeumannKelvin.wavelike(x,y,z),-0.),label="dG/dz",xlabel="xg/U²",levels=-9:2:9,clims=(-10,10))
+contour(-30:0.25:-0,-15:0.125:15,(x,y)->derivative(z ->∫wavelike(z,x-0.5,x+0.5,y-0.5,y+0.5),-0.),label="∫∫dG/dz da",xlabel="xg/U²",levels=-9:2:9,clims=(-10,10))
+contour(-30:0.25:-0,-15:0.125:15,(x,y)->derivative(z ->∫wavelike(z,x-0.5,x+0.5,y-0.5,y+0.5,α=0.1),-0.),label="∫∫dG/dz da",xlabel="xg/U²",levels=-9:2:9,clims=(-10,10))
+contour(-30:0.25:-0,-15:0.125:15,(x,y)->derivative(z ->25∫wavelike(z,x-0.1,x+0.1,y-0.1,y+0.1,α=0.1),-0.),label="∫∫dG/dz da",xlabel="xg/U²",levels=-9:2:9,clims=(-10,10))

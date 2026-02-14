56f453ed6bd462603b2347019c1a6f823b1733c0
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2026-02-14 17:29:26 +0100

    Update benchmark

--- examples/benchmark.jl
+++ examples/benchmark.jl
@@ -1,13 +1,27 @@
 using NeumannKelvin,BenchmarkTools,Test
 
 S(θ,φ) = SA[sin(θ)*cos(φ), sin(θ)*sin(φ), cos(θ)]
-panels = panelize(S, 0, π/2, 0, π, hᵤ=1/64, N_max=Inf) # quite a few panels
+for hᵤ in [1/8,1/16,1/32]
+	panels = panelize(S, 0, π/2, 0, π; hᵤ, N_max=32_000) # quite a few panels
+	println("Test: hᵤ=$hᵤ, N=$(length(panels))")
 
-@btime extrema(cₚ(directsolve!(BodyPanelSystem($panels,sym_axes=(2,3)),verbose=false)))
-@btime extrema(cₚ(gmressolve!(BodyPanelSystem($panels,sym_axes=(2,3)),verbose=false)))
-@btime extrema(cₚ(gmressolve!(BodyPanelSystem($panels,sym_axes=(2,3),wrap=PanelTree),verbose=false)))
-@btime extrema(cₚ(gmressolve!(BodyPanelSystem($panels,sym_axes=(2,3),wrap=PanelTree,θ²=1),verbose=false)))
+	println("Dense directsolve")
+	@btime directsolve!(BodyPanelSystem($panels,sym_axes=(2,3)),verbose=false)
+	println(" Measure")
+	sys = directsolve!(BodyPanelSystem(panels,sym_axes=(2,3)),verbose=false)
+	@btime extrema(cₚ($sys))
+#	@btime steadyforce($sys)
 
-sys = gmressolve!(BodyPanelSystem(panels,sym_axes=(2,3),wrap=PanelTree,θ²=1),verbose=false);
-@btime extrema(cₚ($sys))
-@btime steadyforce($sys)
+	for θ² in [9,1]
+		println("PanelTree θ²=$θ² gmressolve")
+		@btime gmressolve!(BodyPanelSystem($panels,sym_axes=(2,3),wrap=PanelTree;θ²=$θ²),verbose=false)
+		println(" Measure")
+		sys = gmressolve!(BodyPanelSystem(panels,sym_axes=(2,3),wrap=PanelTree;θ²),verbose=false)
+		@btime extrema(cₚ($sys))
+#		@btime steadyforce($sys)
+	end
+end
+
+panels = panelize(S, 0, π/2, 0, π; hᵤ=1/64, N_max=32_000) # quite a few panels
+println("Stress test hᵤ=1/64, N=$(length(panels))")
+@btime extrema(cₚ(gmressolve!(BodyPanelSystem($panels,sym_axes=(2,3),wrap=PanelTree;θ²=4),verbose=false)))
